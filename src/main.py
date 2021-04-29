728ab7f071c77ae514e448f30266b802a14ced32
dlresearchsociety <dl.research.society@gmail.com>
2021-04-29 17:52:42 +0800

    Handle errors correctly

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import atexit
 import sys
 import traceback
 
+from PyQt5.QtWidgets import QApplication
+
 import customlogger as logger
 import initializer
 
@@ -10,19 +12,23 @@ logger.log_to_file()
 
 @atexit.register
 def main_cleanup():
-    logger.info("Virtual Chihiro going to sleep...")
+    logger.info("Virtual Chihiro going back to sleep...")
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    logger.critical("ERROR!")
+    logger.critical(tb)
+    QApplication.quit()
 
 
 def main():
     logger.info("Starting virtual Chihiro...")
 
-    try:
-        initializer.setup(True)
-        from gui.main import setup_gui
+    sys.excepthook = excepthook
+    initializer.setup(True)
+    from gui.main import setup_gui
 
-        app, main = setup_gui(sys.argv)
-        main.show()
-        app.exec_()
-    except:
-        logger.critical("ERROR!")
-        logger.critical(traceback.print_exc())
+    app, main = setup_gui(sys.argv)
+    main.show()
+    app.exec_()
